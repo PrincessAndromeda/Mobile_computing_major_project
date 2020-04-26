bb5cc8923f9404cc28ddac2d890346387a05f160
Jonathan Trimble <s3723735@student.rmit.edu.au>
2020-04-26 19:36:22 +1000

    added Activites for last 2 screens

---
+++ app/src/main/java/com/rmit/major_project/Update_conditions.java
@@ -0,0 +1,13 @@
+package com.rmit.major_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Update_conditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_conditions);
+    }
+}
---
+++ app/src/main/java/com/rmit/major_project/Update_conts.java
@@ -0,0 +1,13 @@
+package com.rmit.major_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Update_conts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_conts2);
+    }
+}
